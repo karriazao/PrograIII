4e79fec05ebe32689d137e6ad6234db04bc3ea8b
[Karina Arriaza] <[karriazao@miumg.edu.gt]>
2026-01-28 11:12:39 -0600

    Programa 1

---
+++ miPrimerPrograma/src/main/java/miprimerprograma/MiPrimerPrograma.java
@@ -0,0 +1,18 @@
+//Karina Alejandra Arriaza Ortiz
+//9959-24-14190
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package miprimerprograma;
+
+/**
+ *
+ * @author daguilae
+ */
+public class MiPrimerPrograma {
+        public static void main(String[] args) {
+            System.out.println("Hola mundo");
+    }
+}
